86fcc21d17e48bd6a68feda8bf7bbe0f4a6ee71e
Nathan Ruhmann <nathan.ruhmann@wwinc.com>
2025-10-13 11:00:28 -0500

    updated activity detection

--- docs/transcription/examples/basic_transcription.py
+++ docs/transcription/examples/basic_transcription.py
@@ -2,7 +2,5 @@ import os
 import whisper
 
 model = whisper.load_model("base")
-
 result = model.transcribe(r"C:\Users\nathanjruhmann\diarscription\docs\reference\audio\sample-a\diarscription-audio-sample-a.mp3")
-
 print(result["text"])
